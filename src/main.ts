7fd65db412c6d5bc39e421cee401edaacb242f4f
JollyGrin <lol@dean.lol>
2026-02-18 03:42:23 +0100

    setup basic vite site

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import * as THREE from 'three'
+
+const canvas = document.getElementById('canvas') as HTMLCanvasElement
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 3
+
+const geometry = new THREE.BoxGeometry()
+const material = new THREE.MeshNormalMaterial()
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+
+function animate() {
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+  renderer.render(scene, camera)
+  requestAnimationFrame(animate)
+}
+animate()
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+})
